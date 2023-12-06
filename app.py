d5ce71b96b2f2545177f8e909e81490bcacfe256
Zar Hlyan Aung <127838777+ZarHlyanAung@users.noreply.github.com>
2023-12-07 03:20:32 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,69 @@
 import streamlit as st
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 import cv2
-import mediapipe as mp
 import numpy as np
-import time
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
-
-def distanceCalculate(p1, p2):
+def distance_calculate(p1, p2):
     """p1 and p2 in format (x1, y1) and (x2, y2) tuples"""
     dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
     return dis
 
+def push_up_count_logic(nose, right_wrist, right_shoulder):
+    if distance_calculate(right_shoulder, right_wrist) < 130:
+        return 1
+    elif push_up_start and distance_calculate(right_shoulder, right_wrist) > 250:
+        return 0
+    else:
+        return 0
+
 def main():
-    st.title("Push-up Counter App")
-    
+    st.title("Push-up Counter using Streamlit and webrtc-streamer")
+
+    webrtc_ctx = webrtc_streamer(
+        key="example",
+        mode=WebRtcMode.SENDRECV,
+        audio=False,
+        video_transformer_factory=None,
+        async_processing=True,
+    )
+
+    if not webrtc_ctx.state.playing:
+        return
+
     push_up_start = 0
     push_up_count = 0
 
-    cap = cv2.VideoCapture(0)  # 0 for default webcam, you can change it to the video file path
+    while True:
+        if webrtc_ctx.video_receiver:
+            frame = webrtc_ctx.video_receiver.recv()
+            image = frame.to_ndarray(format="bgr24")
 
-    with mp_pose.Pose(
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as pose:
+            # Dummy values for demonstration
+            nose = (100, 100)
+            right_wrist = (200, 200)
+            right_shoulder = (150, 150)
 
-        while st.checkbox("Capture Video", value=True):
-            success, image = cap.read()
-            if not success:
-                st.warning("Unable to capture video.")
-                break
+            push_up_count += push_up_count_logic(nose, right_wrist, right_shoulder)
 
-            image_height, image_width, _ = image.shape
-            image.flags.writeable = False
+            # Display the push-up count on the video frame
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            org = (50, 100)
+            font_scale = 1
+            color = (255, 0, 0)
+            thickness = 2
+            image = cv2.putText(
+                image,
+                f"Push-up count: {push_up_count}",
+                org,
+                font,
+                font_scale,
+                color,
+                thickness,
+                cv2.LINE_AA,
+            )
 
-            results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-            
-            nose_point = (int(results.pose_landmarks.landmark[0].x * image_width), 
-                          int(results.pose_landmarks.landmark[0].y * image_height))
-            
-            right_wrist = (int(results.pose_landmarks.landmark[16].x * image_width), 
-                           int(results.pose_landmarks.landmark[16].y * image_height))
-            
-            right_shoulder = (int(results.pose_landmarks.landmark[12].x * image_width), 
-                              int(results.pose_landmarks.landmark[12].y * image_height))
-
-            if distanceCalculate(right_shoulder, right_wrist) < 130:
-                push_up_start = 1
-            elif push_up_start and distanceCalculate(right_shoulder, right_wrist) > 250:
-                push_up_count += 1
-                push_up_start = 0
-
+            # Show the video frame with push-up count
             st.image(image, channels="BGR", use_column_width=True)
-            st.write("Push-up Count:", push_up_count)
-
-            time.sleep(0.01)
-
-    cap.release()
 
 if __name__ == "__main__":
     main()
